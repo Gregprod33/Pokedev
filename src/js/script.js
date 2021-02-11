8836c9877171a5fbb4fc72b5538723539a967c35
PRF <gregoryboes33@gmail.com>
2021-02-11 18:42:06 +0100

    barres animées ok

--- src/js/script.js
+++ src/js/script.js
@@ -5,87 +5,185 @@ var card = $(".devs .dev");
 card.click(function() {
     // Récupération du nom du dev
     let currentDev = $(this).attr("id");
-    let randomAttack = Math.floor(Math.random()*90 + 10)
-    // Liste des brawlers avec leurs statistiques
+
+
+    // Génération des stats aléatoires
+    let randomAttack = Math.floor(Math.random()*80 + 20);
+    let randomDefense = Math.floor(Math.random()*80 + 20);
+    let randomPower = Math.floor(Math.random()*80 + 20);
+
+
+
+    // Liste des dev avec leurs statistiques
     let devs = {
       "Greg": {
         "description": "Greg est un dev passionné !",
-        "image": "testPixel.png",
-        "attack": randomAttack
+        "image": "gabinPixel.png",
+        "attack": randomAttack,
+        "defense": randomDefense,
+        "power": randomPower,
+        "link": "https://www.linkedin.com/in/gr%C3%A9gory-boes-98b0b21a3/"
       },
 
       "Nelson": {
           "description": "Nelson est un dev futé et habile !",
           "image": "fff.png",
-          "attack": randomAttack
+          "attack": randomAttack,
+          "defense": randomDefense,
+          "power": randomPower
       },
 
       "Gabin": {
-        "description": "Gabin use de tous les supterfuges pour arriver à ses fins, c'est un pokedev redoutable et il ne faut surtout pas le sous-estimé, ses points forts sont la ruse, l'intelligence, la souplesse et ses pouvoirs incroyable !!! Sont point faible: les chatouilles !!!",
+        "description": "Gabin use de tous les supterfuges pour arriver à ses fins, c'est un pokedev redoutable ses points forts sont la ruse, l'intelligence, la souplesse et ses pouvoirs incroyable !!! Sont point faible: les chatouilles !!!",
         "image": "gabinPixel.png",
-        "attack": randomAttack
+        "attack": randomAttack,
+        "defense": randomDefense,
+        "power": randomPower
       }
     }
-        $(".dev-name").html(currentDev);
-        $(".dev-description").html(devs[currentDev].description);
-        $(".dev-image").attr("src", "src/img/" + devs[currentDev].image);
-        $("#progressbar").attr("value", devs[currentDev].attack);
-       
-        if(($("#progressbar").val() <= 60) && ($("#progressbar").val() >= 40)) {
-            $("#progressbar").addClass("is-warning");
-        } else if(($("#progressbar").val() <= 40) && ($("#progressbar").val() >= 0)) {
-            $("#progressbar").addClass("is-error");
-        } else if ($("#progressbar").val() > 60) {
-            $("#progressbar").addClass("is-success");
+
+
+    // Récupération des données dans la carte active
+    $(".dev-name").html(currentDev);
+    $(".dev-description").html(devs[currentDev].description);
+    $(".dev-image").attr("src", "src/img/" + devs[currentDev].image);
+    $(".attack").attr("value", devs[currentDev].attack);
+    $(".defense").attr("value", devs[currentDev].defense);
+    $(".power").attr("value", devs[currentDev].power);
+    $(".dev-link").attr("href", devs[currentDev].link);
+
+
+
+    // Couleur des progrssbars en fonction des stats
+    
+    if(($(".attack").val() <= 60) && ($(".attack").val() >= 40)) {
+        $(".attack").addClass("is-warning");
+        $('.progress-value-attack').css("color", "#F7D51D");
+    } else if(($(".attack").val() <= 40) && ($(".attack").val() >= 0)) {
+        $(".attack").addClass("is-error");
+        $('.progress-value-attack').css("color", "#E76E55");
+    } else if ($(".attack").val() > 60) {
+        $(".attack").addClass("is-success");
+        $('.progress-value-attack').css("color", "#92CC41");
+    }
+
+    if(($(".defense").val() <= 60) && ($(".defense").val() >= 40)) {
+        $(".defense").addClass("is-warning");
+        $('.progress-value-defense').css("color", "#F7D51D");
+    } else if(($(".defense").val() <= 40) && ($(".defense").val() >= 0)) {
+        $(".defense").addClass("is-error");
+        $('.progress-value-defense').css("color", "#E76E55");
+    } else if ($(".defense").val() > 60) {
+        $(".defense").addClass("is-success");
+        $('.progress-value-defense').css("color", "#92CC41");
+    }
+    
+
+    if(($(".power").val() <= 60) && ($(".power").val() >= 40)) {
+        $(".power").addClass("is-warning");
+        $('.progress-value-power').css("color", "#F7D51D");
+    } else if(($(".power").val() <= 40) && ($(".power").val() >= 0)) {
+        $(".power").addClass("is-error");
+        $('.progress-value-power').css("color", "#E76E55");
+    } else if ($(".power").val() > 60) {
+        $(".power").addClass("is-success");
+        $('.progress-value-power').css("color", "#92CC41");
+    }
+
+
+
+    // Animation des barres
+
+    const barAnimate = (progressbar) => {
+        let value = progressbar.val();
+        let start = 0;
+        
+            const loading = () => {
+                start += 1;
+                progressbar.val(start);
+                if (start == value) {
+                clearInterval(animate);
+                }
+            };
+            const animate = setInterval(() => loading(), 8);
         }
         
+        let $progressbar1 = $('#progressbar-attack');
+        let $progressbar2 = $('#progressbar-defense');
+        let $progressbar3 = $('#progressbar-power');
+        barAnimate($progressbar1);
+        barAnimate($progressbar2);
+        barAnimate($progressbar3);
         
-       $.fn.devZoomPosition = function() {
-            this.css({
-                "position": "fixed",
-                "left": "50%",
-                "top": "50%",
-                "transform": "translateX(-50%)",
-                "margin-top": -this.height()/2     
-            });     
-        }
+        
+
+        
+        // Positionnement de la carte active au centre
+        
+    $.fn.devZoomPosition = function() {
+        this.css({
+            "position": "fixed",
+            "left": "50%",
+            "top": "50%",
+            "transform": "translateX(-50%)",
+            "margin-top": -this.height()/2     
+        });     
+    }
+
+      
+
+    
+
+
+
+
+        // Animation de la pokeball
+
+    $(".nes-pokeball").animate({ left: "+40%"});
+    $(".nes-pokeball"). animate(
+        {rotation: 360}, { duration: 200, step: function(now, fx) { $(this). css({"transform": "rotate("+now+"deg)"}); } } );
+          
  
     $(".dev-zoom").devZoomPosition();
     $(".dev-zoom").fadeIn();
     $(".dev").css({"opacity": "50%"});
+    $('.progress-value-attack').html(randomAttack + '%');
+    $('.progress-value-defense').html(randomDefense + '%');
+    $('.progress-value-power').html(randomPower + '%');
    
 
-    $(() => {
-        {
-          let progressbar = $('#progressbar');
-          let max = progressbar.attr('max');
-          let time = (300 / max) * 5;
-          let value = progressbar.val();
-          let start = 0;
-      
-          const loading = () => {
-            start += 1;
-            progressbar.val(start);
-      
-            $('.progress-value').html(start + '%');
-      
-            if (start == value) {
-              clearInterval(animate);
-            }
-          };
-          const animate = setInterval(() => loading(), time);
-        };
-      });
-   
  
+
+        
+
+      
+           
+          
+       
+       
+
+
     })
 
+    
+
+    
+
+
+  
 
     $(".dev-zoom i").click(function(){
         $(".dev-zoom").hide();
         $(".dev").css({"opacity": "100%"});
-        $("#progressbar").removeClass("is-warning");
-        $("#progressbar").removeClass("is-success");
-        $("#progressbar").removeClass("is-error");
-
+        $("#progressbar-attack").removeClass("is-warning");
+        $("#progressbar-attack").removeClass("is-success");
+        $("#progressbar-attack").removeClass("is-error");
+        $("#progressbar-defense").removeClass("is-warning");
+        $("#progressbar-defense").removeClass("is-success");
+        $("#progressbar-defense").removeClass("is-error");
+        $("#progressbar-power").removeClass("is-warning");
+        $("#progressbar-power").removeClass("is-success");
+        $("#progressbar-power").removeClass("is-error");
+        $(".nes-pokeball").animate({left: "0%"})
+        $(".nes-pokeball"). animate( {rotation: 0});
     })
